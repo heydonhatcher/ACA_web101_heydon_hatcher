dc39bba01e10cdc84df1489cc784d5dc43200c37
heydonhatcher <heyhatch@gmail.com>
2019-08-24 15:16:05 -0500

    tic tac toe

--- src/scripts/ticTacToe.js
+++ src/scripts/ticTacToe.js
@@ -1,7 +1,43 @@
+var clicks = 1;
+
 var gameMarker = "X";
 
 function changeMarkerToO() {
   gameMarker = "O";
+  console.log("The O button was clicked!");
 }
 
-document.getElementsByClassName("one").innerText = gameMarker;
+function changeMarkerToX() {
+  gameMarker = "X";
+  console.log("The X button was clicked!");
+}
+/*
+function replyClick(ClickedID) {
+  id = ClickedID;
+}
+*/
+
+function changeMarker() {
+  var target = event.target || event.srcElement;
+  var id = target.id;
+  console.log(id);
+  //id = replyClick().id;
+  //console.log(id);
+  console.log("How many clicks? " + clicks);
+  if (clicks % 2 == 0) {
+    changeMarkerToX();
+  }
+
+  if (clicks % 2 != 0) {
+    changeMarkerToO();
+  }
+
+  //document.getElementById(id).innerText = gameMarker;
+
+  document.getElementById(id).innerText = gameMarker;
+
+  console.log(
+    "innerText is: " + document.getElementsByClassName("one").innerText
+  );
+  clicks += 1;
+}

2ddc3724cc88713d51e65c8f729c4e73d19115a0
heydonhatcher <heyhatch@gmail.com>
2019-08-26 16:23:37 -0500

    towers of hanoi

--- src/scripts/towers-of-hanoi.js
+++ src/scripts/towers-of-hanoi.js
@@ -1,1 +1,15 @@
-$(function) 
+$(document).ready(function() {
+  let $block = null;
+
+  $("[data-row]").click(function() {
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+    } else {
+      $block = $(this)
+        .children()
+        .last()
+        .detach();
+    }
+  });
+});
